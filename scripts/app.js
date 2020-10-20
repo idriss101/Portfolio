dbfb047427d58e645883f722d75c765ab02e83a0
Idriss Kone <idriss.kone@icloud.com>
2020-10-20 15:53:50 -0400

    finishin up portfolio, adding some animations

---
+++ scripts/app.js
@@ -0,0 +1,11 @@
+// gsap.registerPlugin(ScrollTrigger);
+
+gsap.from(".header-work-link", {
+  duration: 2,
+  opacity: 0,
+  x: -1000,
+  delay: 1,
+  ease: "back",
+});
+
+// gsap.from("h1", { duration: 1, scale: 0, ease: "power1.out" });
