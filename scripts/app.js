a3940d4308905571d09589f26b4c046c9ba40533
Idriss Kone <idriss.kone@icloud.com>
2020-11-16 17:37:36 -0500

    making site ready to deploy

--- scripts/app.js
+++ scripts/app.js
@@ -1,11 +1,9 @@
 // gsap.registerPlugin(ScrollTrigger);
 
-gsap.from(".header-work-link", {
-  duration: 2,
-  opacity: 0,
-  x: -1000,
-  delay: 1,
-  ease: "back",
-});
+// gsap.from(".header-work-link", {
+//   duration: 1,
+//   opacity: 0,
+//   x: -1000,
+// });
 
 // gsap.from("h1", { duration: 1, scale: 0, ease: "power1.out" });
