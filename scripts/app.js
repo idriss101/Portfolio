09f5917a9ce51881962c29972156a5e4b0bcec8a
Idriss Kone <idriss.kone@icloud.com>
2020-11-17 19:21:38 -0500

    bug fixing

--- scripts/app.js
+++ scripts/app.js
@@ -1,9 +1,1 @@
-// gsap.registerPlugin(ScrollTrigger);
 
-// gsap.from(".header-work-link", {
-//   duration: 1,
-//   opacity: 0,
-//   x: -1000,
-// });
-
-// gsap.from("h1", { duration: 1, scale: 0, ease: "power1.out" });
